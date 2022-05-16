7295263321abcbd46dff72b9c5e3be48a40d1872
EspertoMagento <info@espertomagent.it>
2022-05-16 15:16:45 +0200

    add provincia sud sardegna

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -129,6 +129,7 @@ class InstallData implements InstallDataInterface
             'SI' => 'Siena',
             'SR' => 'Siracusa',
             'SO' => 'Sondrio',
+            'SU' => 'Sud Sardegna',
             'TA' => 'Taranto',
             'TE' => 'Teramo',
             'TR' => 'Terni',
